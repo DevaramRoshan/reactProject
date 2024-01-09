227cd33657d97786a04e21d6af44ab11d167bbb5
DevaramRoshan <147299534+DevaramRoshan@users.noreply.github.com>
2024-01-09 15:12:05 +0000

    done

---
+++ roshan/src/pages/Home.Page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// HOC
+import DefaultlayoutHoc from "../layouts/Default.layout";
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default DefaultlayoutHoc(HomePage);
---
+++ roshan/src/pages/Movie.Page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// HOC
+import MovielayoutHoc from "../layouts/Movie.layout";
+
+const MoviePage = () => {
+  return <div>MoviePage</div>;
+};
+
+export default MovielayoutHoc(MoviePage);
